f32a41b38a4255bcbecac1230cd858ec04b73698
Ian <Ian@E950-B007>
2024-01-09 17:09:27 +0100

    Interfaces y clases

---
+++ PRUEBA_GIT_DAM_DISTANCIA/src/main/java/interfaces/MiInterfaz.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface MiInterfaz {
+
+	public int getEntero();
+	
+}
---
+++ PRUEBA_GIT_DAM_DISTANCIA/src/main/java/modelo/miClase.java
@@ -0,0 +1,13 @@
+package modelo;
+
+import interfaces.MiInterfaz;
+
+public class miClase implements MiInterfaz {
+
+	@Override
+	public int getEntero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
